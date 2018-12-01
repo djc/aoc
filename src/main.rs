c04a7ddfecfafd4bc7e24f6fb0a26fad23aebf0c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-12-01 14:30:24 +0100

    Solved 1-1

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    let val = BufReader::new(File::open("input-1.1.txt")?).lines().fold(0isize, |acc, res| {
+        acc + res.unwrap().parse::<isize>().unwrap()
+    });
+    println!("Value {}", val);
+    Ok(())
+}
