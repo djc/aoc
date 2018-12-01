b04d2b0d92ccaa99eee5bbbdd9030c749970644c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-12-01 14:34:37 +0100

    Rename to better organize the project

--- src/bin/aoc-1a.rs
+++ src/bin/aoc-1a.rs
@@ -2,7 +2,7 @@ use std::io::{BufRead, BufReader};
 use std::fs::File;
 
 fn main() -> std::io::Result<()> {
-    let val = BufReader::new(File::open("input-1.1.txt")?).lines().fold(0isize, |acc, res| {
+    let val = BufReader::new(File::open("input/1.txt")?).lines().fold(0isize, |acc, res| {
         acc + res.unwrap().parse::<isize>().unwrap()
     });
     println!("Value {}", val);
