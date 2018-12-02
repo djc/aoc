338f05dbf4da14758afff35a93b64a4975214aae
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-12-02 13:23:14 +0100

    Format solutions with cargo fmt

--- src/bin/aoc-1a.rs
+++ src/bin/aoc-1a.rs
@@ -1,10 +1,12 @@
-use std::io::{BufRead, BufReader};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()> {
-    let val = BufReader::new(File::open("input/1.txt")?).lines().fold(0isize, |acc, res| {
-        acc + res.unwrap().parse::<isize>().unwrap()
-    });
+    let val = BufReader::new(File::open("input/1.txt")?)
+        .lines()
+        .fold(0isize, |acc, res| {
+            acc + res.unwrap().parse::<isize>().unwrap()
+        });
     println!("Value {}", val);
     Ok(())
 }
--- src/bin/aoc-1b.rs
+++ src/bin/aoc-1b.rs
@@ -4,9 +4,10 @@ use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()> {
     let mut observed = HashSet::new();
-    let values: Vec<isize> = BufReader::new(File::open("input/1.txt")?).lines().map(|ln| {
-        ln.unwrap().parse().unwrap()
-    }).collect();
+    let values: Vec<isize> = BufReader::new(File::open("input/1.txt")?)
+        .lines()
+        .map(|ln| ln.unwrap().parse().unwrap())
+        .collect();
     let val = values.iter().cycle().try_fold(0isize, |acc, new| {
         let res = acc + new;
         let seen = observed.insert(res);
