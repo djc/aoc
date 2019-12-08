61da7742db4c699a259d02b116efa34d243547a8
Dirkjan Ochtman <dirkjan@ochtman.nl>
2019-12-08 08:53:20 +0100

    Day 8, part 2

--- 2019/src/bin/aoc-8.rs
+++ 2019/src/bin/aoc-8.rs
@@ -6,7 +6,11 @@ fn main() -> std::io::Result<()> {
         .chars()
         .map(|c| c.to_digit(10).unwrap())
         .collect::<Vec<_>>();
-    
+    part_2(raw);
+    Ok(())
+}
+
+fn part_1(raw: Vec<u32>) {
     let (mut fewest_zero, mut out) = (std::usize::MAX, 0);
     for i in 0..(raw.len() / 150) {
         let layer = &raw[i * 150..(i + 1) * 150];
@@ -20,6 +24,18 @@ fn main() -> std::io::Result<()> {
     }
     
     println!("out {}", out);
-    
-    Ok(())
+}
+
+fn part_2(raw: Vec<u32>) {
+    let layers = (0..raw.len() / 150)
+        .map(|i| &raw[i * 150..(i + 1) * 150])
+        .collect::<Vec<_>>();
+    for row in 0..6 {
+        for col in 0..25 {
+            let i = row * 25 + col;
+            let p = layers.iter().map(|layer| layer[i]).find(|v| *v != 2).unwrap();
+            print!("{}", p);
+        }
+        println!("");
+    }
 }
