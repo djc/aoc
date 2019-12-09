686595f7bbbfb7b869b760df98860fd52bdd98e3
Dirkjan Ochtman <dirkjan.ochtman@ing.com>
2019-12-09 09:12:16 +0100

    Day 9, part 2

--- 2019/src/bin/aoc-9.rs
+++ 2019/src/bin/aoc-9.rs
@@ -2,22 +2,19 @@ use aoc_2019::intcode::{State, Status};
 
 fn main() -> std::io::Result<()> {
     let state = State::from_file("input/9");
-    part_1(state);
+    part_2(state);
     Ok(())
 }
 
+#[allow(dead_code)]
 fn part_1(mut state: State) {
     state.input.push_back(1);
     assert_eq!(state.run(), Status::Done);
     println!("out {:?}", state.output);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn basic() {
-        //
-    }
+fn part_2(mut state: State) {
+    state.input.push_back(2);
+    assert_eq!(state.run(), Status::Done);
+    println!("out {:?}", state.output);
 }
