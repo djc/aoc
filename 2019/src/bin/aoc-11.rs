13b808cf7ab621917ad2fb042ab7371490317ef0
Dirkjan Ochtman <dirkjan.ochtman@ing.com>
2019-12-11 11:27:35 +0100

    Day 11, part 2

--- 2019/src/bin/aoc-11.rs
+++ 2019/src/bin/aoc-11.rs
@@ -2,76 +2,90 @@ use aoc_2019::intcode::{State, Status};
 use std::collections::HashSet;
 
 fn main() -> std::io::Result<()> {
-    let state = State::from_file("input/11");
-    let mut robot = Robot::new(500);
-    println!("painted {}", robot.run(state));
+    let mut state = State::from_file("input/11");
+    let mut robot = Robot::new(250);
+
+    let mut painted = HashSet::new();
+    let mut panels = HashSet::new();
+    panels.insert(robot.pos);
+    let mut status = Status::NeedInput;
+    while status == Status::NeedInput {
+        painted.insert(robot.pos);
+
+        // Provide input from camera
+        let color = match panels.contains(&robot.pos) {
+            true => 1,
+            false => 0,
+        };
+        state.input.push_back(color as isize);
+        status = state.run();
+
+        let color = state.output.pop_front().unwrap();
+        assert!(color >= 0 && color < 2);
+        if color == 1 {
+            panels.insert(robot.pos);
+        } else {
+            panels.remove(&robot.pos);
+        }
+
+        let turn = state.output.pop_front().unwrap();
+        robot.update(turn);
+        println!(
+            "paint {} turn {} position: {:?} dir {:?}",
+            color, turn, robot.pos, robot.dir
+        );
+        println!("{}", to_string(&panels));
+    }
+
+    println!("painted {}", painted.len());
     Ok(())
 }
 
 struct Robot {
-    size: usize,
-    panels: Vec<u8>,
-    pos: (isize, isize),
+    pos: (usize, usize),
     dir: Dir,
 }
 
 impl Robot {
     fn new(size: usize) -> Self {
         Robot {
-            size,
-            panels: vec![0; size * size],
-            pos: ((size / 2) as isize, (size / 2) as isize),
+            pos: (size, size),
             dir: Dir::Up,
         }
     }
 
-    fn run(&mut self, mut state: State) -> usize {
-        let mut painted = HashSet::new();
-        let mut status = Status::NeedInput;
-        while status == Status::NeedInput {
-            // Provide input from camera
-            let color = self.get(self.pos);
-            state.input.push_back(color as isize);
-            status = state.run();
+    fn update(&mut self, turn: isize) {
+        // Get turn code
+        assert!(turn >= 0 && turn < 2);
+        self.dir.turn(turn);
 
-            // Get color to paint
-            let color = state.output.pop_front().unwrap();
-            assert!(color >= 0 && color < 2);
-            self.set(self.pos, color as u8);
-            painted.insert(self.pos);
-
-            // Get turn code
-            let turn = state.output.pop_front().unwrap();
-            assert!(turn >= 0 && turn < 2);
-            self.dir.turn(turn);
-
-            // Move forward by one panel
-            self.pos = match self.dir {
-                Dir::Up => (self.pos.0, self.pos.1 + 1),
-                Dir::Right => (self.pos.0 + 1, self.pos.1),
-                Dir::Down => (self.pos.0, self.pos.1 - 1),
-                Dir::Left => (self.pos.0 - 1, self.pos.1),
-            };
-            println!("pos {:?}", self.pos);
-        }
-
-        painted.len()
-    }
-
-    fn get(&self, pos: (isize, isize)) -> u8 {
-        self.panels[dbg!(self.index(pos))]
-    }
-
-    fn set(&mut self, pos: (isize, isize), color: u8) {
-        let idx = dbg!(self.index(pos));
-        self.panels[idx] = color;
-    }
-
-    fn index(&self, pos: (isize, isize)) -> usize {
-        (pos.0 as usize + pos.1 as usize * self.size)
+        // Move forward by one panel
+        self.pos = match self.dir {
+            Dir::Up => (self.pos.0, self.pos.1 + 1),
+            Dir::Right => (self.pos.0 + 1, self.pos.1),
+            Dir::Down => (self.pos.0, self.pos.1 - 1),
+            Dir::Left => (self.pos.0 - 1, self.pos.1),
+        };
     }
 }
 
+fn to_string(panels: &HashSet<(usize, usize)>) -> String {
+    let mut s = String::new();
+    // cheat and provide a fixed bounding box
+    for row in (245..=250).rev() {
+        for col in 250..=292 {
+            if panels.contains(&(col, row)) {
+                s.push('#');
+            } else {
+                s.push(' ');
+            }
+        }
+        s.push('\n');
+    }
+    s
+}
+
+#[derive(Debug)]
 enum Dir {
     Up,
     Right,
